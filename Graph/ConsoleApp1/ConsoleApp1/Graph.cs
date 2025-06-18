96460194de5b4d719d8764525732298af256f8f0
pverma1 <pverma1@GUR00455316L>
2025-06-18 10:24:58 +0530

    Graph DFS and BFS solution for string type input

---
+++ Graph/ConsoleApp1/ConsoleApp1/Graph.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Graph
+    {
+        Dictionary<string, List<string>> GraphEdge;
+
+        public Graph()
+        {
+            GraphEdge = new Dictionary<string, List<string>>();
+        }
+
+        public void Addedge(List<(string, string)> edges)
+        {
+            foreach (var (from, to) in edges)
+            {
+                if (!GraphEdge.ContainsKey(from))
+                {
+                    GraphEdge[from] = new List<string>();
+                }
+                if (!GraphEdge.ContainsKey(to))
+                {
+                    GraphEdge[to] = new List<string>();
+                }
+                GraphEdge[from].Add(to);
+                GraphEdge[to].Add(from);
+            }
+        }
+
+
+        public void DFS(string Start)
+        {            
+            Stack<string> stack = new Stack<string>();
+            List<string> visited = new List<string>();
+            stack.Push(Start);
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+
+                if (!visited.Contains(node))
+                {
+                    visited.Add(node);
+                    Console.WriteLine("travel node are " + node);
+                }
+                foreach (var neighbours in GraphEdge[node])
+                {
+                    if (!visited.Contains(neighbours))
+                        stack.Push(neighbours);
+                }
+            }
+        }
+
+        public void BFS(string start)
+        {
+            Queue<string> queue = new Queue<string>();
+            HashSet<string> visited = new HashSet<string>();
+            queue.Enqueue(start);
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+
+                visited.Add(node);
+                Console.WriteLine("travel node are " + node);
+
+                foreach (var neighbours in GraphEdge[node])
+                {
+                    if (!visited.Contains(neighbours))
+                    {
+                        visited.Add(neighbours);
+                        queue.Enqueue(neighbours);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Graph/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+Graph g = new Graph();
+var v = new List<(string, string)>()
+{
+    ("A","B"),
+    ("B","C"),
+    ("B","H"),
+    ("C","E"),
+    ("H","E"),
+    ("C","D"),
+    ("E","F"),
+    ("E","G")
+};
+g.Addedge(v);
+g.DFS("A");
+g.BFS("A");
+Console.ReadLine();
